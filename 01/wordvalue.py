954a2ba086f7dae6c39d4f93e851bb340f9a97db
Larry Polk <larrydpolk@gmail.com>
2019-03-12 15:08:38 -0500

    Added fixes

--- 01/wordvalue.py
+++ 01/wordvalue.py
@@ -3,6 +3,7 @@ from data import DICTIONARY, LETTER_SCORES
 
 def load_words():
     """Load dictionary into a list and return list"""
+
     words = None
     with open(DICTIONARY, 'r') as f:
         words = [line.strip() for line in f]
@@ -13,19 +14,15 @@ def load_words():
 def calc_word_value(word):
     """Calculate the value of the word entered into function
     using imported constant mapping LETTER_SCORES"""
+
     return sum([LETTER_SCORES.get(c.upper(), 0) for c in word])
 
 
 def max_word_value(words=None):
     """Calculate the word with the max value, can receive a list
     of words as arg, if none provided uses default DICTIONARY"""
-    if not words:
-        words = load_words()
 
-    scores = [(calc_word_value(word), word) for word in words]
-    scores.sort(key=lambda t: t[0], reverse=True)
-
-    return scores[0][1]
+    return max(words or load_words(), key=lambda word: calc_word_value(word))
 
 
 if __name__ == "__main__":
